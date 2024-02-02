0a0fa485fabdaed7472785d387222a1b728fb4b9
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2024-02-02 11:32:28 -0300

    fetch: add modal new transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
-import { HeaderButton, HeaderContainer, HeaderContent } from './styles'
-
+import * as Dialog from '@radix-ui/react-dialog'
 import logoSvg from '../../assets/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
+import { HeaderButton, HeaderContainer, HeaderContent } from './styles'
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoSvg} alt="" />
-        <HeaderButton>Nova transação</HeaderButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <HeaderButton>Nova transação</HeaderButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
